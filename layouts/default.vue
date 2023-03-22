60544f0c5f25dd67120d12f8b2cd443479885ff9
Chamel <chatmethar2@gmail.com>
2023-03-22 21:07:51 +0700

    start portal 2

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav>
+            <li>a</li>
+            <li>a</li>
+            <li>a</li>
+        </nav>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-primary">
+        Protal
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
